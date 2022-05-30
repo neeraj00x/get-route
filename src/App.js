df3e0595516bc22b0ebec5d619d2c133379f9804
Neeraj Pratap Singh <nprataps@iitk.ac.in>
2022-05-31 03:28:25 +0530

    inial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { useSelector } from "react-redux";
+import { withScriptjs, withGoogleMap } from "react-google-maps";
+import Map from "./Map";
 
-function App() {
+const Home = () => {
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
+  const formattedOrigin = {
+    latitude: 25.900000,
+    longitude: 81.839996,
+  };
+  const formattedDestination = {
+    latitude: 26.900000,
+    longitude: 83.839996,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WrappedMap
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing,places&key=AIzaSyC7pyg2XfsHis1DNOlMrCB-nx41-a49qJ0`}
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: "80vh" }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      formattedOrigin={formattedOrigin}
+      formattedDestination={formattedDestination}
+    />
   );
-}
+};
 
-export default App;
+export default Home;
